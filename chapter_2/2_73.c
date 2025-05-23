8058947b9d8c4424ee25cf25295c8da716a17d78
lalafua <qtexpsem@gmail.com>
2025-05-23 23:58:53 +0800

    5.23

---
+++ chapter_2/2_73.c
@@ -0,0 +1,13 @@
+/*
+ * Write code for function with the following prototype:
+ * 
+ * // Addition that saturates to TMin ot TMax 
+ * int saturating_add(int x, int y);
+ *
+ * Instead of overflowing the way normal two's-complement addition does, sat-
+ * urating addition returns `TMax` when there would be positive overflow, and TMin
+ * when there would be negative overflow. Saturating arithmetic is commanly used 
+ * in programs that perform digital signal processing.
+ * 
+ * Your function should follow the bit--level integer coding rules (page 120).
+ */
